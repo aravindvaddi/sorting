fa8f6ed9f41fe5e0c7da21d89247907d59006afb
Aravind Vaddi <aravindvaddi.github@gmail.com>
2017-07-02 22:12:27 +0530

    Changes: - function to implement bubble sort for integers - bug fixes

--- sorting.c
+++ sorting.c
@@ -29,6 +29,40 @@ void sort_selection (int *array, int size)
 		}
 
 		/* swapping minimum value with the value at first index */
-		swap (&a[c1], &a[minimum]);
+		swap (&array[c1], &array[minimum]);
+	}
+}
+
+/* function to implement selection sort on given array of integers */
+
+void sort_bubble (int *array, int size)
+{
+	/* declaring variable to use for counting */
+	int c1, c2;
+
+	/* declaring variable for checking for completion */
+	int flag;
+
+	/* traversing through the array size - 1 times */
+	for (c1 = 0; c1 < size - 1; c1++)
+	{
+		/* setting flag to 0, implying that no elements have been swapped */
+		flag = 0;
+
+		/* traversing through the array and moving the largest element to the end */
+		for (c2 = 0; c2 < size - c1 - 1; c2++)
+		{
+			/* comparing consecutive elements, each element is checked twice except the first and last */
+			if (array[c2] > array[c2 + 1])
+			{
+				/* flag is set to 1 to imply an element swap has occured */
+				flag = 1;
+				swap (&array[c2], &array[c2 + 1]);
+			}
+		}
+
+		/* if flag is not set we break from the loop as this implies no elements have been swapped, i.e the array is sorted */
+		if(!flag)
+			break;
 	}
 }
--- sorting.h
+++ sorting.h
@@ -4,5 +4,5 @@
 void swap (int *a, int *b);
 
 void sort_selection (int *array, int size);
-
+void sort_bubble (int *array, int size);
 #endif
